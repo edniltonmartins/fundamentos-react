e4abb922c81dfd02d2c08d086e874a8dab869442
edniltonmartins <ednilton17@gmail.com>
2020-05-25 01:04:35 -0300

    Passando parametros para um componente

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import First from './components/basics/First'
+import WithParameter from './components/basics/WithParameters'
 
 ReactDOM.render(
     <div>
         <First/>
+        <WithParameter 
+            subtitle="Its cool" 
+            title="Second Component"/>
     </div>, 
     document.getElementById('root'))
