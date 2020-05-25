b9b2543a033f0a07320cee82d3af4013409f4c88
edniltonmartins <ednilton17@gmail.com>
2020-05-25 00:48:48 -0300

    Incluindo um primeiro componente

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import First from './components/basics/First'
 
-
-//const el = document.getElementById('root')
-const tag = <strong>Olá React!</strong>
 ReactDOM.render(
-    <div>{ tag }</div>, 
+    <div>
+        <First/>
+    </div>, 
     document.getElementById('root'))
